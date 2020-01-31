3887fe8cd08682e2862d3e00699adb6d9c18f924
whua898 <46870956+whua898@users.noreply.github.com>
2020-01-31 15:47:57 +0800

    Update rootssh.sh

--- rootssh.sh
+++ rootssh.sh
@@ -5,3 +5,4 @@ passwd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 reboot
+
